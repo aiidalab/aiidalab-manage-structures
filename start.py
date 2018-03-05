6ee5d9004485c398e3cd44acf373a8c4eebcc5e3
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2018-03-05 16:16:37 +0100

    Add start.py file

---
+++ start.py
@@ -0,0 +1,17 @@
+import ipywidgets as ipw
+
+def get_start_widget(appbase, jupbase):
+    #http://fontawesome.io/icons/
+    template = """
+    <table>
+    <td valign="top"><ul>
+    <li><a href="{appbase}/upload_structure.ipynb" target="_blank">Upload structures</a>
+    </ul></td>
+
+    </tr></table>
+"""
+    
+    html = template.format(appbase=appbase, jupbase=jupbase)
+    return ipw.HTML(html)
+    
+#EOF
