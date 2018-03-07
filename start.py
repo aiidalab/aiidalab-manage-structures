1cc2066158fc01947b82924d54534996a432f474
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2018-03-07 15:53:29 +0100

    Upload from CoD now works

--- start.py
+++ start.py
@@ -8,7 +8,7 @@ def get_start_widget(appbase, jupbase):
     <li><a href="{appbase}/upload_structure.ipynb" target="_blank">Upload structures</a>
     </ul></td>
     <td valign="top"><ul>
-    <li><a href="{appbase}/upload_from_csd.ipynb" target="_blank">Upload structures from CSD</a>
+    <li><a href="{appbase}/import_from_cod.ipynb" target="_blank">Upload structures from CoD</a>
     </ul></td>
     </tr></table>
 """
