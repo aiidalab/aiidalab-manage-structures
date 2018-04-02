35c9b6cc36dedc50a4202313d1d81d467a90d500
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2018-04-02 20:30:36 +0000

    Add examples section

--- start.py
+++ start.py
@@ -4,11 +4,21 @@ def get_start_widget(appbase, jupbase):
     #http://fontawesome.io/icons/
     template = """
     <table>
+    <tr>
+        <th style="text-align:center"></th>
+        <th style="width:70px" rowspan=2></th>
+        <th style="text-align:center"></th>
+        <th style="width:70px" rowspan=2></th>
+        <th style="text-align:center"></th>
+    <tr>
     <td valign="top"><ul>
-    <li><a href="{appbase}/upload_structure.ipynb" target="_blank">Upload structures</a>
+    <li><a href="{appbase}/examples.ipynb" target="_blank">Look at the examples</a>
     </ul></td>
     <td valign="top"><ul>
-    <li><a href="{appbase}/import_from_cod.ipynb" target="_blank">Upload structures from CoD</a>
+    <li><a href="{appbase}/import_from_cod.ipynb" target="_blank">Upload from the CoD</a>
+    </ul></td>
+    <td valign="top"><ul>
+    <li><a href="{appbase}/upload_structure.ipynb" target="_blank">Upload from computer</a>
     </ul></td>
     </tr></table>
 """
